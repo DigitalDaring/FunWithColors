6acbf0279b56c84d490bdd8479f117893e596327
Edmund Bates <edmund.bates@carvana.com>
2022-03-11 16:59:15 -0700

    This patch removes some of the new packages that just arent needed

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { hydrate, render } from "react-dom";
 
 declare global {
   namespace JSX {
